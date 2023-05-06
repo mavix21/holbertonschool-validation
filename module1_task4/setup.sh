5500915fea75832be56e5f462da6d64f8f8f821b
mavix21 <marcevizcarra2109@hotmail.com>
2023-05-05 20:53:11 -0500

    Build essentials

--- module1_task4/setup.sh
+++ module1_task4/setup.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-apt-get update && apt-get install -y git make wget && wget https://github.com/gohugoio/hugo/releases/download/v0.84.0/hugo_extended_0.84.0_Linux-64bit.deb && dpkg -i hugo_extended_0.84.0_Linux-64bit.deb
+apt-get update && apt-get install -y git make wget build-essential && wget https://github.com/gohugoio/hugo/releases/download/v0.84.0/hugo_extended_0.84.0_Linux-64bit.deb && dpkg -i hugo_extended_0.84.0_Linux-64bit.deb
 
 # Build the website
 make build
