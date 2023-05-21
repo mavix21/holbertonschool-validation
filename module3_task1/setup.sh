965785660db1023ec8f39f5653460a0cb2687758
mavix21 <marcevizcarra2109@hotmail.com>
2023-05-21 14:20:34 -0500

    Feat: Add shellcheck installation

--- module3_task1/setup.sh
+++ module3_task1/setup.sh
@@ -7,3 +7,4 @@ sudo apt-get install -y wget
 sudo wget https://github.com/gohugoio/hugo/releases/download/v0.84.0/hugo_extended_0.84.0_Linux-64bit.deb && sudo dpkg -i hugo_extended_0.84.0_Linux-64bit.deb
 sudo npm install -g markdownlint-cli
 sudo npm install -g markdown-link-check
+sudo apt install shellcheck
