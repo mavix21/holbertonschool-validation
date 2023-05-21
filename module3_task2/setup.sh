42eee64b84ff0f8e66af126953579bc0b877e992
mavix21 <marcevizcarra2109@hotmail.com>
2023-05-21 18:46:55 -0500

    Feat: Install golangci-lint and add go path to system path

--- module3_task2/setup.sh
+++ module3_task2/setup.sh
@@ -9,3 +9,14 @@ sudo npm install -g markdownlint-cli
 sudo npm install -g markdown-link-check
 sudo apt install shellcheck
 sudo apt-get -y install zip
+sudo apt-get -y install python3-pip
+python3 -m pip install requests
+sudo apt-get -y install git
+curl -sSfL https://raw.githubusercontent.com/golangci/golangci-lint/master/install.sh | sh -s -- -b $(go env GOPATH)/bin
+
+# include /usr/local/go/bin && (go env GOPATH)/bin in PATH
+export PATH=$PATH:/usr/local/go/bin
+export PATH=$PATH:$(go env GOPATH)/bin
+
+git clone https://github.com/holbertonschool/W3C-Validator.git
+sudo mv W3C-Validator /opt/
